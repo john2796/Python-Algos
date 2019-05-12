2f5ba736fa0d3f487abd394db51fc8108b2b19b2
john2796 <jbmiranda22796@gmail.com>
2019-05-12 04:29:19 -0700

    binary search recursive

--- sorting-algos/binary_search_recursive.py
+++ sorting-algos/binary_search_recursive.py
@@ -1,2 +1,7 @@
 def binary_search_recursive(arr, target, low, high):
+    low = 0
+    high = arr(len) - 1
     pass
+
+    print(binary_search_recursive([1, 2, 3, 4, 5, 6, 21, 22,
+                                   23, 24, 25, 26, 31, 32, 33, 34, 35, 36], 35))
