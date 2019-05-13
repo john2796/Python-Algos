e8e4db2071279a278efb851d3f8aec6fe38eef9a
john2796 <jbmiranda22796@gmail.com>
2019-05-12 21:00:00 -0700

    k

--- sorting-algos/binary_search_recursive.py
+++ sorting-algos/binary_search_recursive.py
@@ -1,7 +1,24 @@
-def binary_search_recursive(arr, target, low, high):
-    low = 0
-    high = arr(len) - 1
-    pass
+def bsr(arr, target, low, high):
+    mid = (low + high) // 2
+    if high > low:
+        return -1
+    if target == arr[mid]:
+        return True
+    elif target < arr[mid]:
+        return bsr(arr, target, low, mid - 1)
+    else:
+        return bsr(arr, target, mid + 1, high)
+    # elif target < arr[mid]:
+    #     high = mid - 1
+    # else:
+    #     low = mid + 1
 
-    print(binary_search_recursive([1, 2, 3, 4, 5, 6, 21, 22,
-                                   23, 24, 25, 26, 31, 32, 33, 34, 35, 36], 35))
+
+print(bsr([1, 2, 3, 4, 5, 6, 21, 22], 35, 0, 8))
+
+
+#  1 Solve the base case by thinking of the simplest examples you can solve
+#  2 Solve the recursive case, either by using a top-down or bottom-up approach
+#  3 Analyze the runtime and space complexity
+#  4 Write the pseudocode
+#  5 Write the code
