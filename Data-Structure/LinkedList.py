82453309a112d738a24e369abaf06c03ecbc8747
john2796 <jbmiranda22796@gmail.com>
2019-05-13 07:35:13 -0700

    added single linked list

--- Data-Structure/LinkedList.py
+++ Data-Structure/LinkedList.py
@@ -15,33 +15,66 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
         self.size = 0
 
     def display(self):
-        if not self.head:
-            return -1
+        if not self.head and not self.tail:
+            return None
         else:
             elems = []
-            old_head = self.head
-            while old_head:
-                elems.append(old_head.value)
-                old_head = self.head.next
+            cur = self.head
+            while cur:
+                elems.append(cur.value)
+                cur = cur.next
             return elems
+
         pass
 
     def enqueue(self, item):
+        new_node = Node(item)
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.size += 1
+        return
 
-        pass
+    def dequeue(self):
+        if not self.head and not self.tail:
+            return None
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+        self.size -= 1
+        return
 
-    def dequeue(self, item):
-        pass
+    def len(self):
+        return self.size
 
-    def len(self, item):
-        pass
-
-    def contains(self, item):
-        pass
+    def contains(self, target):
+        if not self.head and not self.tail:
+            return None
+        else:
+            cur = self.head
+            while cur:
+                cur = cur.next
+                if target == cur.value:
+                    return True
+                else:
+                    return False
 
 
 test = LinkedList()
+print(test.enqueue(1))
+print(test.enqueue(2))
+print(test.enqueue(3))
+print(test.enqueue(4))
+print(test.enqueue(5))
+print(test.contains(6), 'contains -->')
 print(test.display())
+print(test.len())
