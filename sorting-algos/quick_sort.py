38266bd9e1653923dfaff00081ccb35658927410
john2796 <jbmiranda22796@gmail.com>
2019-05-20 07:46:29 -0700

    add check for less than and equal check

--- sorting-algos/quick_sort.py
+++ sorting-algos/quick_sort.py
@@ -3,26 +3,35 @@
 arr = [17, 41, 5, 22, 54, 6, 29, 3, 13]
 # iterative solution
 
-#print(swapp(arr, 0, len(arr) - 1))
+# print(swapp(arr, 0, len(arr) - 1))
 
 
-def quick_sort(arr):
+def quick_sort(arr, low, high):
+    if low <= high:
+        pi = partition(arr, low, high)
+        print(pi, 'testing')
+        quick_sort(arr, low, pi - 1)
+        quick_sort(arr, pi + 1, high)
     pass
 
 
 def partition(arr, low, high):
     pivot = arr[high]
     i = (low - 1)
-    for j in range(0, len(high) - 1):
-        print(j)
+    print(arr, 'before', i)
+    for j in range(low, high):
+        print(arr, "counter i:", i, "counter j:", j, arr[j], "<=", pivot)
+        if arr[j] <= pivot:
+            i += 1
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+
+    temp = arr[i + 1]
+    arr[i + 1] = arr[high]
+    arr[high] = temp
+    return (i + 1)
 
 
-def swapp(arr, l, r):
-    temp = arr[l]
-    arr[l] = arr[r]
-    arr[r] = temp
-    return arr
-
-
-print(quick_sort(arr))
-print(partition(arr, 0, len(arr)))
+# print(quick_sort(arr, 0, len(arr) - 1))
+print(quick_sort(arr, 0, len(arr) - 1))
