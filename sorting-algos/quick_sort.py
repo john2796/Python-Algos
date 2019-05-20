4fa9839f76505ae968e707206f66b9366c255111
john2796 <jbmiranda22796@gmail.com>
2019-05-20 08:14:17 -0700

    added comments

--- sorting-algos/quick_sort.py
+++ sorting-algos/quick_sort.py
@@ -8,25 +8,32 @@ arr = [17, 41, 5, 22, 54, 6, 29, 3, 13]
 
 def quick_sort(arr, low, high):
     if low <= high:
+      # partition function every call of the function will execute
         pi = partition(arr, low, high)
-        print(pi, 'testing')
+        # left side of arr smaler
         quick_sort(arr, low, pi - 1)
+        # right side of arr larger
         quick_sort(arr, pi + 1, high)
+        print(
+            f"partition:{pi} --->, low:{low},---> high:{high}, ---->  arr:{arr}")
     pass
 
 
 def partition(arr, low, high):
+  # grab last item as pivot
     pivot = arr[high]
+    # starts at negative 1
     i = (low - 1)
     print(arr, 'before', i)
+    # loop through starting to end
     for j in range(low, high):
-        print(arr, "counter i:", i, "counter j:", j, arr[j], "<=", pivot)
+        # if j counter is less than or equal pivot swap
         if arr[j] <= pivot:
             i += 1
             temp = arr[i]
             arr[i] = arr[j]
             arr[j] = temp
-
+    # swap i + 1 to last item
     temp = arr[i + 1]
     arr[i + 1] = arr[high]
     arr[high] = temp
