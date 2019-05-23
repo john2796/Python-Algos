e484e32d0e2a4bb2f46160161108df488b68a153
john2796 <jbmiranda22796@gmail.com>
2019-05-22 20:51:06 -0700

    halfway done

--- Data-Structure/double_linkedlist.py
+++ Data-Structure/double_linkedlist.py
@@ -19,9 +19,11 @@ class DoublyLinkedList:
         else:
             elem = []
             current = self.head
+            print(
+                f"head: {self.head.value} tail: {self.tail.value} size: {self.size}")
             while current:
                 elem.append(current.value)
-                current = current
+                current = current.next
             return elem
 
     def add_to_head(self, item):
@@ -30,22 +32,66 @@ class DoublyLinkedList:
             self.head = new_node
             self.tail = new_node
         else:
-            cur = self.head
+            current = self.head
+            current.prev = new_node
             self.head = new_node
-            self.head.next = cur
+            self.head.next = current
+            self.head.prev = None
         self.size += 1
-        return
+        return self.head
 
-    def add_to_tail(self):
-        pass
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        if not self.tail and not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            current = self.tail
+            current.next = new_node
+            self.tail = new_node
+            self.tail.prev = current
+        self.size += 1
+        return self.head
 
+# 3 cases for removing
+# 1 if there's no Node
+# 2 if there's only one node
+# 3 if there's more than 2 Node
     def remove_from_head(self):
-        pass
+        if not self.head and not self.tail:
+            return None
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            current = self.head.next
+            self.head = current
+            self.head.prev = None
+        self.size -= 1
+        return self.head
 
     def remove_from_tail(self):
-        pass
+        if not self.head and not self.tail:
+            return None
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            current = self.tail.prev
+            self.tail = current
+            self.tail.next = None
+        self.size -= 1
+        return self.head
 
-    def remove_to_front(self):
+
+# * `move_to_front` takes a reference to a node in the list and moves it to the front of the list, shifting all other list nodes down.
+# ------------------------------------------------
+# 3 cases for removing
+# 1 if there's no Node
+# 2 if there's only one node
+# 3 if there's more than 2 Node
+
+    def remove_to_front(self, item):
         pass
 
     def remove_to_end(self):
@@ -59,7 +105,12 @@ class DoublyLinkedList:
 
 
 test = DoublyLinkedList()
+test.add_to_head(3)
+test.add_to_head(2)
 test.add_to_head(1)
-test.add_to_head(4)
-test.add_to_head(23)
+test.add_to_tail(4)
+test.remove_from_head()
+test.remove_from_tail()
+
+
 print(test.display())
